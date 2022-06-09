39ed4848edddf67e5122cde1b02d1525736d571c
GSpaki <gspaki@gmail.com>
2022-06-09 20:26:22 -0300

    feat: Add resource domain layer

---
+++ lib/modules/resources/domain/entities/resource_entity.dart
@@ -0,0 +1,40 @@
+class ResourceEntity {
+  final DateTime createdDate;
+  final DateTime updatedDate;
+  final String resourceId;
+  final String moduleId;
+  final String languageId;
+  final String value;
+
+  ResourceEntity({
+    required this.createdDate,
+    required this.updatedDate,
+    required this.resourceId,
+    required this.moduleId,
+    required this.languageId,
+    required this.value,
+  });
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is ResourceEntity &&
+        other.createdDate == createdDate &&
+        other.updatedDate == updatedDate &&
+        other.resourceId == resourceId &&
+        other.moduleId == moduleId &&
+        other.languageId == languageId &&
+        other.value == value;
+  }
+
+  @override
+  int get hashCode {
+    return createdDate.hashCode ^
+        updatedDate.hashCode ^
+        resourceId.hashCode ^
+        moduleId.hashCode ^
+        languageId.hashCode ^
+        value.hashCode;
+  }
+}
---
+++ lib/modules/resources/domain/i_usecases/i_get_all_resources_usecase.dart
@@ -0,0 +1,8 @@
+import 'package:oxidized/oxidized.dart';
+
+import '../../../shared/failures/i_failure.dart';
+import '../entities/resource_entity.dart';
+
+abstract class IGetAllResourcesUsecase {
+  Future<Result<List<ResourceEntity>, IFailure>> call();
+}
