a7b0866d0102c50d3f117a0e38d02605fa062e96
GSpaki <gspaki@gmail.com>
2022-06-09 18:46:34 -0300

    feat: Add app module

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+import 'modules/app/app.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Container(),
-    );
-  }
+  return runApp(ModularApp(module: AppModule(), child: const AppWidget()));
 }
