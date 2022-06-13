66453ac9bd376e859f24c7c3e09072edd8d409c9
Spaki <64546843+GSpaki@users.noreply.github.com>
2022-06-13 09:53:32 -0300

    build: Add hive package

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 import 'modules/app/app.dart';
 
-void main() {
+Future<void> main() async {
+  await Hive.initFlutter();
   return runApp(ModularApp(module: AppModule(), child: const AppWidget()));
 }
